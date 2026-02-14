f46ab6cceae8797b0b26a7f91c19bdcaa7016cec
JohnCard <johncard962@gmail.com>
2026-02-13 23:53:49 -0600

    carousel update

--- scripts/list.js
+++ scripts/list.js
@@ -17,7 +17,6 @@ document.addEventListener('DOMContentLoaded', (e) => {
 const stockForm = document.getElementById('stock-form')
 //todo modal
 const modalElement = document.getElementById('exampleModal')
-// const modal = bootstrap.Modal.getOrCreateInstance(modalElement)
 //* Save data button
 const galleryDataButton = document.getElementById('button-gallery')
 galleryDataButton.addEventListener('click', saveGalleryData)
@@ -31,6 +30,76 @@ for(let item of gallery){
 }
 
 
+//* carousel
+const track = document.querySelector(".carousel-track");
+const viewport = document.querySelector(".carousel-viewport");
+
+let currentIndex = 0;
+let visibleItems = 4; // provisional
+let imageWidth = 0;
+
+function renderImages(images, container) {
+    images.forEach(item => {
+        const img = document.createElement("img");
+        img.src = item.img;
+        img.alt = item.name;
+        container.appendChild(img);
+    });
+}
+
+renderImages(gallery, track)
+
+function updateVisibleItems() {
+    const width = window.innerWidth; 
+
+    if (width >= 1200) visibleItems = 5;
+    else if (width >= 768) visibleItems = 4;
+    else visibleItems = 2;
+}
+
+function updateImageWidth() {
+    const viewportWidth = viewport.offsetWidth;
+    imageWidth = viewportWidth / visibleItems;
+
+    const imgs = document.querySelectorAll(".carousel-track img");
+    imgs.forEach(img => {
+        img.style.width = `${imageWidth}px`;
+        img.style.height = `${250}px`
+        img.classList.add('px-30px')
+    });
+}
+
+function moveCarousel() {
+    const offset = currentIndex * imageWidth;
+    track.style.transform = `translateX(-${offset}px)`;
+}
+
+updateVisibleItems();
+updateImageWidth();
+
+//* carousel window event
+window.addEventListener("resize", () => {
+    updateVisibleItems();
+    updateImageWidth();
+    moveCarousel();
+});
+
+document.querySelector(".right").addEventListener("click", () => {
+    if (currentIndex < gallery.length - visibleItems) {
+        currentIndex++;
+    }
+    moveCarousel();
+});
+
+document.querySelector(".left").addEventListener("click", () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    }
+    moveCarousel();
+});
+
+//! carousel ends
+//* main gallery logic & container
 row.addEventListener('click', (e) => {
     let user = returnUser()
     const tagContent = e.target.textContent
